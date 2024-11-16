e965b2bcf2dc1e9ab26630bb34770aba53df2ade
Deo silaen <deokeldisilaen@gmail.com>
2024-11-16 15:41:00 +0700

    updateBom

--- RunnerLabyrinthEscape/Assets/Scripts/BombSpawn.cs
+++ RunnerLabyrinthEscape/Assets/Scripts/BombSpawn.cs
@@ -6,6 +6,8 @@ public class BombSpawn : MonoBehaviour
 {
     public GameObject bombPrefab;
     private float spawnRate;
+    private int speed = 3;
+    private int Poinst;
 
     void Update()
     {
@@ -13,6 +15,25 @@ public class BombSpawn : MonoBehaviour
 
         if(Random.value < spawnRate * Time.deltaTime){
             Instantiate(bombPrefab, new Vector3(Random.Range(-0.82f, 1.07f), 3.57f, 0), Quaternion.identity);
+        }
+
+        MovementBom();
+
+    }
+
+    void MovementBom(){
+        transform.position += Vector3.down * speed *Time.deltaTime;  
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            Poinst = FindAnyObjectByType<PointScoring>().Points;
+            
+            if(Poinst <= 0){
+                GameOver.instance.ShowGameOver();
+            } else {
+               Poinst -= 5;
+            }
         }
     }
 }
--- RunnerLabyrinthEscape/Assets/Scripts/PointScoring.cs
+++ RunnerLabyrinthEscape/Assets/Scripts/PointScoring.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class PointScoring : MonoBehaviour
 {
     public Text TextPoint; 
-    private int Points = 0;
+    public int Points = 0;
    
    private void OnTriggerEnter2D(Collider2D other) {
     if(other.CompareTag("Points")){
