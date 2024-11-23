ae819d3485030d03d28b38464f9a3b8323c87609
Deo silaen <deokeldisilaen@gmail.com>
2024-11-24 02:06:44 +0700

    addMusickBackground

--- RunnerLabyrinthEscape/Assets/Scripts/AudioManager.cs
+++ RunnerLabyrinthEscape/Assets/Scripts/AudioManager.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] AudioSource MusicSource;
     [SerializeField] AudioSource SFXSource;
 
+    public AudioClip background;
     public AudioClip Scoring;
     public AudioClip Shield;
     public AudioClip Boom;
 
-
+    private void Start() {
+        MusicSource.clip = background;
+        MusicSource.Play();
+    }
 
     public void PlaySFX(AudioClip clip){
         SFXSource.PlayOneShot(clip);
