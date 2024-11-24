66afaf58a8c08b341655434d35e576a5684746a1
Deo silaen <deokeldisilaen@gmail.com>
2024-11-24 12:41:47 +0700

    addSceneMenu

---
+++ RunnerLabyrinthEscape/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void DisableAnimation()
+    {
+        if (animator != null)
+        {
+            
+            animator.enabled = false;
+        }
+    }
+
+    public void EnableAnimation()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("play");
+        }
+    }
+}
