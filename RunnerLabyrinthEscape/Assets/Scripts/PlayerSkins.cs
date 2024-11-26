d14ad6a27f3e0aa0b571da83e8c0bb25d42f39ed
Deo silaen <deokeldisilaen@gmail.com>
2024-11-26 15:34:38 +0700

    updateMenu

--- RunnerLabyrinthEscape/Assets/Scripts/PlayerSkins.cs
+++ RunnerLabyrinthEscape/Assets/Scripts/PlayerSkins.cs
@@ -19,11 +19,9 @@ public class PlayerSkins : MonoBehaviour
 
         if (SkinManager.Instance != null && SkinManager.Instance.skins != null) {
             if (selectedSkinIndex >= 0 && selectedSkinIndex < SkinManager.Instance.skins.Length) {
-                 Debug.Log("Applying skin: " + selectedSkinIndex);
                 playerRenderer.sprite = SkinManager.Instance.skins[selectedSkinIndex];
             } else {
                 playerRenderer.sprite = SkinManager.Instance.skins[DefaultSkinIndex];
-                Debug.LogWarning("Invalid skin index, default skin applied.");
             }
         } else {
             if (defaultSprite != null) {
